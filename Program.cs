b766bb9888a5574d56b66cdcb6c81ebf3c25ab16
Little-Pink-Dragon <111520046+Little-Pink-Dragon@users.noreply.github.com>
2022-09-08 03:00:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+ int a = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите второе число: ");
+ int b = int.Parse(Console.ReadLine());
+ if (a > b)
+ {
+    Console.WriteLine("{0} Наибольшее число", a);
+ }
+ else
+ {
+    Console.WriteLine("{0} Наибольшее число ", b);
+ }
+ Console.ReadLine();
+ 
